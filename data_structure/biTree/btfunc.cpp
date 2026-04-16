a3a6fc7b9a976232b8e2d50e5f56039df5af1ee4
dailiduzhou <qzhsdream@163.com>
2026-04-16 17:43:19 +0800

    feat:biTree commit

--- data_structure/biTree/btfunc.cpp
+++ data_structure/biTree/btfunc.cpp
@@ -1,9 +1,48 @@
 #include "bitree.h"
-
+#include <cstddef>
+#include <iostream>
+using namespace std;
 void Preorder(BiTree T, void (*visit)(BiTree)) {
   if (T) {
     visit(T);
     Preorder(T->lchild, visit);
     Preorder(T->rchild, visit);
+  }
+}
+
+void CreatrebiTree(BiTree &T) {
+  TElemType ch;
+  cin >> ch;
+  if (ch == '#') {
+    T = NULL;
+  } else {
+    T = new BiNode;
+    T->data = ch;
+    CreatrebiTree(T->lchild);
+    CreatrebiTree(T->rchild);
+  }
+}
+
+void BiTreeDepth(const BiTree &T, int h, int &depth) {
+  if (T) {
+    if (h > depth) {
+      depth = h;
+    }
+    BiTreeDepth(T->lchild, h + 1, depth);
+    BiTreeDepth(T->rchild, h + 1, depth);
+  }
+}
+
+int BiTreeDepth(const BiTree &T) {
+  if (T) {
+    int hL = BiTreeDepth(T->lchild);
+    int hR = BiTreeDepth(T->rchild);
+    if (hL >= hR) {
+      return hL + 1;
+    } else {
+      return hR + 1;
+    }
+  } else {
+    return 0;
   }
 }
